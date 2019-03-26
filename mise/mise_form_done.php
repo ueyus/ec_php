05636f89b34bbfa115610bef204ecdf3bf47de0a
an <an@localhost.localdomain>
2019-03-26 17:13:45 +0900

    six-two commit

--- mise/mise_form_done.php
+++ mise/mise_form_done.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+	session_regenerate_id(true);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,24 +11,84 @@
 <body>
 <?php
 
-require_once('../common/common.php');
+try {
+	require_once('../common/common.php');
 
-$post = sanitize($_POST);
+	$post = sanitize($_POST);
 
-$onamae = $post['onamae'];
-$email = $post['email'];
-$postal1 = $post['postal1'];
-$postal2 = $post['postal2'];
-$address = $post['address'];
-$tel = $post['tel'];
+	$onamae = $post['onamae'];
+	$email = $post['email'];
+	$postal1 = $post['postal1'];
+	$postal2 = $post['postal2'];
+	$address = $post['address'];
+	$tel = $post['tel'];
 
-print $onamae . '様<br>';
-print 'ご注文ありがとう<br>';
-print $email . 'にメールを送りましたので、ご確認お願いします。<br>';
-print '商品は以下の住所へ発送させていただきます。<br>';
-print $postal1 . '-' . $postal2 . '<br>';
-print $address . '<br>';
-print $tel . '<br>';
+	print $onamae . '様<br>';
+	print 'ご注文ありがとう<br>';
+	print $email . 'にメールを送りましたので、ご確認お願いします。<br>';
+	print '商品は以下の住所へ発送させていただきます。<br>';
+	print $postal1 . '-' . $postal2 . '<br>';
+	print $address . '<br>';
+	print $tel . '<br>';
+
+	$honbun = '';
+	$honbun .= $onamae . "様　\nこのたびはご注文ありがとうございました。\n";
+	$honbun .= "\n";
+	$honbun .= "ご注文商品\n";
+	$honbun .= "-------------\n";
+
+	$cart = $_SESSION['cart'];
+	$count = $_SESSION['count'];
+	$max = count($cart);
+
+	$dsn = 'mysql:dbname=shop;host=localhost';
+	$user = 'root';
+	$password = '';
+	$dbh = new PDO($dsn, $user, $password);
+	$dbh->query('SET NAMES utf8');
+
+	for ($i = 0; $i < $max; $i++) {
+		$sql = 'select name, price from mst_product where code=?';
+		$stmt = $dbh->prepare($sql);
+		$data[0] = $cart[$i];
+		$stmt->execute($data);
+
+		$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		$name = $rec['name'];
+		$price = $rec['price'];
+		$suryo = $count[$i];
+		$shokei = $price * $suryo;
+
+		$honbun .= $name . '';
+		$honbun .= $price . '円　x';
+		$honbun .= $suryo . '個　=';
+		$honbun .= $shokei . "円\n";
+	}
+
+	$dbh = null;
+
+	$honbun .= "送料は無料です。\n";
+	$honbun .= "-------------------\n";
+	$honbun .= "\n";
+	$honbun .= "代金は以下の口座にお振込みください。\n";
+	$honbun .= "テスト銀行　テスト支店　口座1111111111\n";
+	$honbun .= "入金確認が取れ次第、梱包、発送させていただきます。\n";
+	$honbun .= "\n";
+	$honbun .= "◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇\n";
+	$honbun .= "　～安心野菜の〇〇農園～　\n";
+	$honbun .= "\n";
+	$honbun .= "〇〇県〇市\n";
+	$honbun .= "電話：　22222222\n";
+	$honbun .= "FAX：　33333333333\n";
+	$honbun .= "\n";
+	$honbun .= "◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇\n";
+
+
+} catch (Exception $e) {
+	print 'ただいま障害によりご迷惑をおかけしています。';
+	exit();
+}
 
 ?>
 
